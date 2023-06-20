2c34a057c7fe798e942387b8defee68783eb76a4
Hallison <hallison.o.b@gmail.com>
2023-06-20 11:36:54 -0300

    feat: users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://0.0.0.0/nest'), AuthModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://0.0.0.0/nest'),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
