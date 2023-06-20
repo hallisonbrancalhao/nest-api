7d10690ad2cd0f4ba6add8461277c043091cd7f0
Hallison <hallison.o.b@gmail.com>
2023-06-20 11:36:12 -0300

    feat: auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://0.0.0.0/nest')],
+  imports: [MongooseModule.forRoot('mongodb://0.0.0.0/nest'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
